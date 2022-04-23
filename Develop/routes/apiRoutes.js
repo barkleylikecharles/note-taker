784b961c7e58002591f6ca25209a9f7f20bdc3ad
Nicole Barkley <barkleylikecharles@icloud.com>
2022-04-23 13:01:11 -0400

    update files package.json

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,5 @@
 // Add dependencies
-const { v4: uuidv4 }= require('uuid')
+// const { v4: uuidv4 }= require('uuid')
 const router = require('express').Router();
 
 const saveDatabase = require('../db/db');
@@ -14,7 +14,7 @@ router.post('/' , (req, res) => {
         res.status(400).json({msg: 'Please enter both a title and text description to complete.'})
     } else {
         const newJSON = {
-            id: uuidv4(),
+            // id: uuidv4(),
             title: newTitle,
             text: newText
         };
