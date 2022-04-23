482469deff05239011c7d2bc9bcf3d513cac281b
Nicole Barkley <barkleylikecharles@icloud.com>
2022-04-22 23:35:11 -0400

    working apiRoute and start html route

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -23,12 +23,5 @@ router.post('/' , (req, res) => {
     };
     });
 
-// POST request
-// router.post('/notes', (req, res) => {
-//     saveDatabase
-//         .addNote(req.body)
-//         .then((note) => res.json(note))
-//         .catch(err => res.status(500).json(err));
-// });
 
 module.exports = router;
