7cf26cd67ebb3c9689331f863d1d5a737e91baca
Nicole Barkley <barkleylikecharles@icloud.com>
2022-04-22 23:07:49 -0400

    heroku push

--- routes/apiRoutes/notes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const noteRoutes = require('../apiRoutes/notes');
-
-router.use(noteRoutes);
-router.use(require('./Routes'));
-
-module.exports = router;
