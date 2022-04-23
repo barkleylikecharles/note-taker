f9c53eafa75bc19809d35207a9299eb74013b0eb
Nicole Barkley <barkleylikecharles@icloud.com>
2022-04-22 23:22:50 -0400

    heroku push

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,24 @@
+// Add dependencies
+const { v4: uuidv4 }= require('uuid')
+const router = require('express').Router();
+
+const saveDatabase = require('../db/db');
+
+// GET request
+router.get('/', (req, res) => res.json(saveDatabase));
+
+router.post('/' , (req, res) => {
+    const newTitle = req.body.title;
+    const newText = req.body.text;
+    if (!newTitle || !newText) {
+        res.status(400).json({msg: 'Please enter both a title and text description to complete.'})
+    } else {
+        const newJSON = {
+            id: uuidv4(),
+            title: newTitle,
+            text: newText
+        };
+        saveDatabase.push(newJSON);
+        res.json(data);
+    };
+    });
